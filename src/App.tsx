9aadc7673321ccb3878f54a827f02967c2e6093e
Hunter-Smsm <ziad35199@gmail.com>
2024-12-07 15:38:47 +0200

    adding loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import image from "./assets/0-floor.png";
 import Filter from "./components/Filter/Filter";
 import { SVGContext } from "./context/SharingData";
 import PolygonDetails from "./components/PolygonDetails/PolygonDetails";
+import Loading from "./components/Loading/Loading";
 
 function App() {
 	const svgElement = useContext(SVGContext);
-
+	const [loading, setLoading] = useState(true);
 	// Replace the svg holder with the svg element when it is fetched
 	useEffect(() => {
 		if (svgElement) {
-			const svgHolder: Element | null = document.querySelector("main .svg-holder");
+			const svgHolder: Element | null =
+				document.querySelector("main .svg-holder");
 			svgHolder && svgHolder.replaceWith(svgElement);
 		}
 	}, [svgElement]);
 
 	return (
-		<main>
+		<main
+			onLoad={() => {
+				setLoading(false);
+			}}
+		>
+			{loading && svgElement && <Loading />}
 			<img src={image} />
 			<svg className="svg-holder"></svg>
 			<Filter />
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import "./loading.css";
+
+function Loading() {
+	return (
+		<div className="loading-container">
+			<span></span>
+		</div>
+	);
+}
+
+export default Loading;
